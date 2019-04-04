99ddd304a4b6cb9cf3880d2783d1555763c0be7c
Neil Scheuermann <neilscheuermann@gmail.com>
2019-04-04 09:29:45 -0600

    Home button returns home. Added some styling.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
 class NavBar extends React.Component {
   movieCategory = React.createRef()
@@ -7,26 +8,39 @@ class NavBar extends React.Component {
     console.log(this.movieCategory.current.value)
   }
 
+  clickedHome = () => {
+    this.props.history.push('/')
+  }
+
   render() {
     return (
       <header className="navbar">
         <h1>MovieView</h1>
-        <select
-          name="movies"
-          defaultValue=""
-          ref={this.movieCategory}
-          onChange={() => this.handleChange()}
-        >
-          <option disabled value="">
-            Movie Categories
-          </option>
-          <option value="popularity">Popular</option>
-          <option value="vote-average">Top Rated</option>
-          <option value="release_date">New Releases</option>
-        </select>
+        <div className="navbar-right">
+          <select
+            className="navbar-right-items"
+            name="movies"
+            defaultValue=""
+            ref={this.movieCategory}
+            onChange={this.handleChange}
+          >
+            <option disabled value="">
+              Movie Categories
+            </option>
+            <option value="popularity">Popular</option>
+            <option value="vote-average">Top Rated</option>
+            <option value="release_date">New Releases</option>
+          </select>
+          <h2
+            className="navbar-right-items home-btn"
+            onClick={this.clickedHome}
+          >
+            Home
+          </h2>
+        </div>
       </header>
     )
   }
 }
 
-export default NavBar
+export default withRouter(NavBar)
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,22 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import WelcomePage from './WelcomePage'
 import PopularMovies from './PopularMovies'
 
 const Routes = () => {
   return (
     <div className="main-content">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={WelcomePage} />
-          <Route
-            exact
-            path="/popular/desc/:pageNum"
-            component={PopularMovies}
-          />
-          <Route render={() => <Redirect to="/" />} />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/popular/desc/:pageNum" component={PopularMovies} />
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
     </div>
   )
 }
