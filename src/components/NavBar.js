13b4df1e159f51e6ae9dff9d62c7fc2f6cf81f9a
Neil Scheuermann <neilscheuermann@gmail.com>
2019-04-11 16:21:55 -0600

    removed drop-down from navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,28 +16,9 @@ class NavBar extends React.Component {
     return (
       <header className="navbar">
         <h1>MovieView</h1>
-        <div className="navbar-right">
-          <select
-            className="navbar-right-items"
-            name="movies"
-            defaultValue=""
-            ref={this.movieCategory}
-            onChange={this.handleChange}
-          >
-            <option disabled value="">
-              Movie Categories
-            </option>
-            <option value="popularity">Popular</option>
-            <option value="vote-average">Top Rated</option>
-            <option value="release_date">New Releases</option>
-          </select>
-          <h2
-            className="navbar-right-items home-btn"
-            onClick={this.clickedHome}
-          >
-            Home
-          </h2>
-        </div>
+        <h2 className="navbar-right-items home-btn" onClick={this.clickedHome}>
+          Home
+        </h2>
       </header>
     )
   }
