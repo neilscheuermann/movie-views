d59b07997ecee8696fa277425522471949b4c3b1
Neil Scheuermann <neilscheuermann@gmail.com>
2019-04-15 16:56:27 -0600

    Formatted movie info card.

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { numberWithCommas } from '../helpers'
 
 const MovieInfo = props => {
   const {
@@ -8,17 +9,37 @@ const MovieInfo = props => {
     release_date,
     vote_average,
     vote_count,
+    poster_path,
   } = props.movie
 
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>Popularity Rating: {popularity}</p>
-      <p>Overview: {overview}</p>
-      <p>Release Date: {release_date}</p>
-      <p>
-        Rating: {vote_average}/10 ({vote_count} total votes)
-      </p>
+    <div className="movie-info">
+      <div>
+        <img
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+              : '/images/default_poster.jpg'
+          }
+          alt=""
+          className="poster-img"
+        />
+      </div>
+      <div className="movie-info-card-right">
+        <div>
+          <h2>
+            {title} ({release_date.split('-')[0]})
+          </h2>
+          <p>{overview}</p>
+        </div>
+        <div className="scores-ratings">
+          <p>Popularity Score: {popularity}</p>
+          <p>
+            Viewer Rating: {vote_average}/10 ({numberWithCommas(vote_count)}{' '}
+            total votes)
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
